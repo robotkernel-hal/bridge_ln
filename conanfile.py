b310455b496d2a6159036b3579401a7cc7003b8c
Robert Burger <robert.burger@dlr.de>
2023-11-22 06:26:58 +0100

    updated to conan2 syntax

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools
+from conan import ConanFile
 
 class MainProject(ConanFile):
     python_requires = "conan_template/[~=5]@robotkernel/stable"
@@ -6,13 +6,12 @@ class MainProject(ConanFile):
 
     name = "bridge_ln"
     description = "The ln bridge exports robotkernel services via links-and-nodes."
-    exports_sources = ["*", "!.gitignore"] + ["!%s" % x for x in tools.Git().excluded_files()]
+    exports_sources = ["*", "!.gitignore"]
+
+    tool_requires = "links_and_nodes_base_python/[>=1.2.3 <3]@common/stable"
 
     def requirements(self):
+        self.requires("robotkernel/[~=5]@robotkernel/stable")
         self.requires("libstring_util/[~=1]@common/stable")
-        self.requires("liblinks_and_nodes/[>=1.2.3 <3]@common/stable") 
-    
-    def build_requirements(self):
-        self.build_requires("ln_helper/[~=0]@robotkernel/stable")
-        self.build_requires("robotkernel/[~=5]@robotkernel/stable")
-
+        self.requires("liblinks_and_nodes/[>=1.2.3 <3]@common/stable")
+        self.requires("ln_helper/[~=0]@robotkernel/stable")
