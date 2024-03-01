ace038e9df20a0a5216b68a4791daa72e4b52de6
Maximilian Mühlbauer <maximilian.muehlbauer@dlr.de>
2024-03-01 11:53:40 +0100

    fix: conan2 compatible version ranges

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~=5]@robotkernel/stable"
+    python_requires = "conan_template/[~5]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "bridge_ln"
@@ -11,7 +11,7 @@ class MainProject(ConanFile):
     tool_requires = "links_and_nodes_base_python/[>=1.2.3 <3]@common/stable"
 
     def requirements(self):
-        self.requires("robotkernel/[~=5]@robotkernel/stable")
-        self.requires("libstring_util/[~=1]@common/stable")
+        self.requires("robotkernel/[~5]@robotkernel/stable")
+        self.requires("libstring_util/[~1]@common/stable")
         self.requires("liblinks_and_nodes/[>=1.2.3 <3]@common/stable")
-        self.requires("ln_helper/[~=0]@robotkernel/stable")
+        self.requires("ln_helper/[~0]@robotkernel/stable")
