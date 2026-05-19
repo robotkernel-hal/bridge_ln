4e3988860ad0194b4f1cb7aeb0fe5f002fe24630
Robert Burger <robert.burger@dlr.de>
2026-05-19 09:05:03 +0200

    change: using stable deps

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conan import ConanFile
 
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~6]@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "bridge_ln"
@@ -12,9 +12,9 @@ class MainProject(ConanFile):
     tool_requires = "links_and_nodes_base_python/[>=1.2.3 <3]@common/stable"
 
     def requirements(self):
-        self.requires("robotkernel/[~6]@robotkernel/unstable")
+        self.requires("robotkernel/[~6]@robotkernel/stable")
         self.requires("libstring_util/[~1]@common/stable")
         self.requires("liblinks_and_nodes/[>=1.2.3 <3]@common/stable")
-        self.requires("robotkernel_ln_helper/[~6]@robotkernel/unstable")
+        self.requires("robotkernel_ln_helper/[~6]@robotkernel/stable")
 
 
